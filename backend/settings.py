641b2cc125fe383d36984361532dce820a95494c
deza395 <debora.ed.zarate@gmail.com>
2021-04-12 12:06:47 -0300

    db

--- backend/settings.py
+++ backend/settings.py
@@ -126,16 +126,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
- """
-
  DATABASES = {
+
     'default': {
+
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd2fnc0ura7lsnb',
         'USER': 'kvzbdlwypxwqzs',
