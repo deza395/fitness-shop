41382e077fd3e1adbc96134850aeedd70747e89a
deza395 <debora.ed.zarate@gmail.com>
2021-04-12 02:03:19 -0300

    first commit

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+import { register } from '../actions/userActions'
+
+
+function RegisterScreen({ location, history }) {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+
+    const dispatch = useDispatch()
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    const userRegister = useSelector(state => state.userRegister)
+    const { error, loading, userInfo } = userRegister
+
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        if (password != confirmPassword) {
+            setMessage('Las contraseñas no coinciden')
+        } else {
+            dispatch(register(name, email, password))
+        }
+
+    }
+
+    return (
+        <FormContainer>
+            <h1>Registrate</h1>
+            {message && <Message variant='danger'>{message}</Message>}
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+
+                <Form.Group controlId='name'>
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control
+                        required
+                        type='name'
+                        placeholder='Ingrese su nombre'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        required
+                        type='email'
+                        placeholder='Ingrese su Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Ingrese Contraseña'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='passwordConfirm'>
+                    <Form.Label>Confirme Contraseña</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Confirme Contraseña'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant='primary'>
+                    Registrese
+                </Button>
+
+            </Form>
+
+            <Row className='py-3'>
+                <Col>
+                    Ya tenes una cuenta? <Link
+                        to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+                        ingresá aqui
+                        </Link>
+                </Col>
+            </Row>
+        </FormContainer >
+    )
+}
+
+export default RegisterScreen
